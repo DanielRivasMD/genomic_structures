7c95ee8812d1196f26155936e7ca201d0a35f7c9
DanielRivasMD <danielrivasmd@gmail.com>
2021-06-10 21:31:04 +0200

    update function macros. effective genome & lambda calculator

--- src/macros/functions.rs
+++ src/macros/functions.rs
@@ -1,4 +1,3 @@
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /// calculate effective genome length with default parameters: BIN_SIZE & BIN_OVERLAP
@@ -27,26 +26,6 @@ macro_rules! lambda_calculator {
   // default values
   ( $pr: expr, $egl: expr ) => {
     lambda_calculator( $pr, $egl, BIN_SIZE as f64 )
-  };
-
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-macro_rules! chr_counter {
-
-  // mobile element chromosomal counter
-  ( $read_id: expr, $read_count: expr, $chr_pair: expr, $position_hm: expr ) => {
-    let binned_position = format!("{}", $chr_pair.binner());
-    chr_counter( $read_id, $position_hm, binned_position );
-    $read_count += 1;
-  };
-
-  // structural variant chromosomal counter
-  ( $read_id: expr, $sv_pair: expr, $position_hm: expr ) => {
-    let binned_pos = ( $sv_pair.read1.chr_read.binner(), $sv_pair.read2.chr_read.binner() );
-    let binned_position = format!("{}-{}", std::cmp::min(binned_pos.0, binned_pos.1), std::cmp::max(binned_pos.0, binned_pos.1));
-    chr_counter( $read_id, $position_hm, binned_position );
   };
 
 }
