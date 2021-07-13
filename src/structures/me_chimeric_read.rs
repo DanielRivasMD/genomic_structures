cf3dec4ec8fc512983413f8f89e19b4b9bb895fd
DanielRivasMD <danielrivasmd@gmail.com>
2021-07-13 11:34:56 +0200

    update mobile element chimeric read

--- src/structures/me_chimeric_read.rs
+++ src/structures/me_chimeric_read.rs
@@ -1,7 +1,12 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// standard libraries
+use std::fmt;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 // crate utilities
-use crate::utils::structures::{
+use crate::structures::{
   break_point::BreakPoint,
   chr_anchor::ChrAnchor,
   me_anchor::MEAnchor,
@@ -9,18 +14,22 @@ use crate::utils::structures::{
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// annotate primary (index 0) & secondary aligned reads
+/// Contain primary (index 0) and secondary aligned reads annotation.
 #[derive(Debug, new, Default)]
 pub struct MEChimericRead {
+  /// Sequence.
   #[new(default)]
   pub sequence: String,
 
+  /// Mobile element read.
   #[new(default)]
   pub me_read: Vec<MEAnchor>,
 
+  /// Chromosomal read.
   #[new(default)]
   pub chr_read: Vec<ChrAnchor>,
 
+  /// Breakpoint.
   #[new(default)]
   pub breakpoint: BreakPoint,
 }
@@ -28,7 +37,19 @@ pub struct MEChimericRead {
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 impl MEChimericRead {
-  // reverse complement sequence
+  /// Obtain reverse complement sequence.
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// use genomic_structures::MEChimericRead;
+  ///
+  /// let mut toreverse = MEChimericRead::new();
+  /// toreverse.sequence = "GATTACA".to_string();
+  /// let reversed = "TGTAATC".to_string();
+  ///
+  /// assert_eq!(toreverse.sequence_reverser(), reversed);
+  /// ```
   pub fn sequence_reverser(&self) -> String {
     self
       .sequence
@@ -52,8 +73,7 @@ impl MEChimericRead {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-use std::fmt;
-
+// display trait implementation
 impl fmt::Display for MEChimericRead {
   fn fmt(
     &self,
@@ -66,14 +86,5 @@ impl fmt::Display for MEChimericRead {
     )
   }
 }
-
-// #[new(default)]
-// pub sequence: String,
-// #[new(default)]
-// pub me_read: Vec<MEAnchor>,
-// #[new(default)]
-// pub chr_read: Vec<ChrAnchor>,
-// #[new(default)]
-// pub breakpoint: BreakPoint,
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
