230a99926c026f622665f81fbf495d4d3a76fde0
DanielRivasMD <danielrivasmd@gmail.com>
2021-08-15 09:16:56 +0200

    segregate private function lambda calculate

---
+++ src/functions/thresholder/lambda_calculate.rs
@@ -0,0 +1,37 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+use crate::BIN_SIZE;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+use super::lambda_calculate;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+macro_rules! lambda_calculate {
+  ( $function: ident;
+    params |> $noreads: expr, $eflen: expr;
+    expected |> $expected: expr;
+  ) => {
+    #[test]
+    fn $function() {
+      let lambda = lambda_calculate($noreads, $eflen, BIN_SIZE as f64);
+      assert_eq!(
+        lambda,
+        $expected,
+        "\n\nNumber of reads: {:?}.\nEffective scaffold length: {:?}.\nCalculated lambda (λ): {:?}.\nExpected: {:?}.\n\n",
+        $noreads,
+        $eflen,
+        lambda,
+        $expected,
+      );
+    }
+  };
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+lambda_calculate!(test01; params |> 100., 400.; expected |> 25.;);
+lambda_calculate!(test02; params |> 5050., 75400000.; expected |> 0.006_697_612_732_095_490_5;);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
