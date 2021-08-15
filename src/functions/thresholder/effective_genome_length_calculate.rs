a950b5597b2c0f47d70f2e7e7d8299008eb76f4c
DanielRivasMD <danielrivasmd@gmail.com>
2021-08-15 09:16:34 +0200

    segregate private function effective genome length calculate

---
+++ src/functions/thresholder/effective_genome_length_calculate.rs
@@ -0,0 +1,40 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+use crate::{
+  BIN_OVERLAP,
+  BIN_SIZE,
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+use super::effective_genome_length_calculate;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// TODO: rename
+macro_rules! effective_genome_length_calculate {
+  ( $function: ident;
+    $glen: expr, $expected: expr
+  ) => {
+    #[test]
+    fn $function() {
+      let effective_length = effective_genome_length_calculate(
+        $glen,
+        BIN_SIZE as f64,
+        BIN_OVERLAP as f64,
+      );
+      assert_eq!(
+        effective_length, $expected,
+        "\n\nScaffold length: {:?}.\nEffective scaffold length: {:?}.\nExpected: {:?}.\n\n",
+        $glen, effective_length, $expected,
+      );
+    }
+  };
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+effective_genome_length_calculate!(test01; 2000., 4000.);
+effective_genome_length_calculate!(test02; 3243556456., 6487112912.);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
