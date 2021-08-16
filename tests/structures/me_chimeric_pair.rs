b42043e3cd6dfa8b463e5db753a7bff3651d113c
DanielRivasMD <danielrivasmd@gmail.com>
2021-08-16 06:48:04 +0200

    test mobile element chimeric pair macro

--- tests/structures/me_chimeric_pair.rs
+++ tests/structures/me_chimeric_pair.rs
@@ -21,12 +21,35 @@ data_test! {
 
     assert_eq!(retrieved.sequence, expected);
   }
+macro_rules! me_chimeric_pair {
+  ( $function: ident;
+    $sequence: expr, $expected: expr
+  ) => {
+    #[test]
+    fn $function() {
+      let mut to_retrieve = MEChimericPair::new();
+      to_retrieve.read1.sequence = $sequence;
+      let retrieved = to_retrieve.get_chr_anchor();
+      assert_eq!(
+        retrieved.sequence, $expected,
+        "\n\nInput sequence: {:?}.\nRetrieved sequence: {:?}.\nExpected: {:?}.\n\n",
+        $sequence, retrieved.sequence, $expected,
+      );
+    }
+  };
+}
 
   - _00 ("GATTACA".to_string(), "GATTACA".to_string())
   - _01 ("AAAAAAA".to_string(), "AAAAAAA".to_string())
   - _02 ("MACTHAA".to_string(), "MACTHAA".to_string())
   - _03 ("CAAGAAC".to_string(), "CAAGAAC".to_string())
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 }
+// test
+me_chimeric_pair!(test01; String::from("AAAAAAA"), String::from("AAAAAAA"));
+me_chimeric_pair!(test02; String::from("MACTHAA"), String::from("MACTHAA"));
+me_chimeric_pair!(test03; String::from("CAAGAAC"), String::from("CAAGAAC"));
+me_chimeric_pair!(test04; String::from("GATTACA"), String::from("GATTACA"));
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
