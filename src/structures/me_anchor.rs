d7d1c37bf0b9ffc6cfa01e1d04e41fc982afa8aa
DanielRivasMD <danielrivasmd@gmail.com>
2021-08-16 10:09:42 +0200

    implement calculate break point mobile element anchor

--- src/structures/me_anchor.rs
+++ src/structures/me_anchor.rs
@@ -200,6 +200,70 @@ impl MEAnchor {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+impl MEAnchor {
+  ///
+  /// Calculate break point.
+  ///
+  /// # Parameters
+  ///
+  /// * `sequence` - Original read sequence.
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// use genomic_structures::{
+  ///   BreakPoint,
+  ///   MEAnchor,
+  ///   OrientationEnum,
+  ///   CIGAR,
+  /// };
+  ///
+  /// let cigar = "15S15M";
+  /// let flag = 83;
+  /// let mobel = String::from("mobel77");
+  /// let orientation = OrientationEnum::None;
+  /// let position = 1;
+  /// let size = 11000.;
+  ///
+  /// let mut me_anchor = MEAnchor::load(
+  ///   CIGAR::load(cigar, position).unwrap(),
+  ///   flag,
+  ///   mobel.clone(),
+  ///   orientation,
+  ///   position,
+  ///   size,
+  /// );
+  ///
+  /// me_anchor.calculate_break_point("MMMM0987654321B1234567890OOOOO");
+  ///
+  /// let mut manual = MEAnchor::new();
+  /// manual.breakpoint = BreakPoint {
+  ///   sequence:   String::from("MMMM0987654321B1234567890"),
+  ///   coordinate: 15.,
+  /// };
+  ///
+  /// assert_eq!(me_anchor.breakpoint, manual.breakpoint);
+  /// ```
+  pub fn calculate_break_point(
+    &mut self,
+    sequence: &str,
+  ) {
+    // TODO: consider implementing tagging
+    if self.cigar.left_boundry <= 0 && self.interpret(5) {
+      self
+        .breakpoint
+        .update(sequence, self.cigar.left_boundry as f64);
+    } else if self.cigar.right_boundry > self.size as i32 && !self.interpret(5)
+    {
+      self
+        .breakpoint
+        .update(sequence, self.cigar.right_boundry as f64 - self.size);
+    }
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 // implement SAM flag
 impl SAMFlag for MEAnchor {
   fn get_flag(&self) -> i32 {
