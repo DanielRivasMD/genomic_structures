f262dfadcc747369d74ae14e9c175ed425c67790
DanielRivasMD <danielrivasmd@gmail.com>
2021-08-16 06:46:13 +0200

    test mobile element chimeric read macro

--- tests/structures/me_chimeric_read.rs
+++ tests/structures/me_chimeric_read.rs
@@ -17,12 +17,35 @@ data_test! {
     toreverse.sequence = sequence;
     assert_eq!(toreverse.sequence_reverser(), expected);
   }
+macro_rules! me_chimeric_read {
+  ( $function: ident;
+    $sequence: expr, $expected: expr
+  ) => {
+    #[test]
+    fn $function() {
+      let mut to_reverse = MEChimericRead::new();
+      to_reverse.sequence = $sequence;
+      let reversed = to_reverse.reverse_sequence();
+      assert_eq!(
+        reversed, $expected,
+        "\n\nInput sequence: {:?}.\nReversed sequence: {:?}.\nExpected: {:?}.\n\n",
+        $sequence, reversed, $expected,
+      );
+    }
+  };
+}
 
   - _00 ("GATTACA".to_string(), "TGTAATC".to_string())
   - _01 ("AAAAAAA".to_string(), "TTTTTTT".to_string())
   - _02 ("MACTHAA".to_string(), "TTHAGTM".to_string())
   - _03 ("CAAGAAC".to_string(), "GTTCTTG".to_string())
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 }
+// test
+me_chimeric_read!(test01; String::from("AAAAAAA"), String::from("TTTTTTT"));
+me_chimeric_read!(test02; String::from("MACTHAA"), String::from("TTHAGTM"));
+me_chimeric_read!(test03; String::from("CAAGAAC"), String::from("GTTCTTG"));
+me_chimeric_read!(test04; String::from("GATTACA"), String::from("TGTAATC"));
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
