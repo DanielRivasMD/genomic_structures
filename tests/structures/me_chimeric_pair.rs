47c150fdb7a4df92bea6bf9a7521d564d4f8eb0b
DanielRivasMD <danielrivasmd@gmail.com>
2021-08-23 15:44:47 +0200

    expand test mobile element chimeric pair

--- tests/structures/me_chimeric_pair.rs
+++ tests/structures/me_chimeric_pair.rs
@@ -71,35 +71,115 @@ macro_rules! test_tag {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// test
-test_tag!(tag00;
+// fail
+test_tag!(tag00; // none
+  expect |> None;
+  vargs ... 0 => None, 0 => None;
+  vargs ... 0 => None, 0 => None;
+);
+
+test_tag!(tag01; // ambigous ends
   expect |> None;
   vargs ... 50 => Upstream, 75 => Upstream;
+  vargs ... 1000 => Downstream;
+);
+
+test_tag!(tag02; // palindromic reads
+  expect |> None;
+  vargs ... 0 => None, 75 => Upstream;
+  vargs ... 1050 => Downstream, 75 => Upstream;
+);
+
+test_tag!(tag03; // palindromic reads
+  expect |> None;
+  vargs ... 50 => Downstream, 75 => Upstream;
   vargs ... 50 => Upstream, 75 => Upstream;
 );
 
-test_tag!(tag01;
+// test upstream
+test_tag!(tag10; // upstream same position both reads
+  expect |> None;
+  vargs ... 100 => Upstream, 50 => Upstream;
+  vargs ... 50 => Upstream, 75 => Upstream;
+);
+
+test_tag!(tag11; // minimum read1
   expect |> Read1;
   vargs ... 100 => Upstream, 50 => Upstream;
   vargs ... 150 => Upstream, 75 => Upstream;
 );
 
-test_tag!(tag02;
+test_tag!(tag12; // minimum read2
   expect |> Read2;
   vargs ... 100 => Upstream, 150 => Upstream;
   vargs ... 150 => Upstream, 75 => Upstream;
 );
 
-test_tag!(tag03;
+test_tag!(tag13; // resilient secondary alignments
   expect |> Read1;
-  vargs ... 10995 => Downstream, 10500 => Downstream;
-  vargs ... 10950 => Downstream, 10900 => Downstream;
+  vargs ... 100 => Upstream, 50 => Upstream, 10000 => Downstream;
+  vargs ... 150 => Upstream, 75 => Upstream, 0 => None;
 );
 
-test_tag!(tag04;
+test_tag!(tag14;
+  expect |> Read2; // alined same position physical read
+  vargs ... 100 => Upstream, 150 => Upstream, 0 => None, 0 => Palindromic;
+  vargs ... 150 => Upstream, 75 => Upstream, 75 => Upstream;
+);
+
+test_tag!(tag15; // umapped pair read2
   expect |> Read2;
+  vargs ... 150 => Upstream, 75 => Upstream, 75 => Upstream;
+  vargs ... 0 => None, 0 => None;
+);
+
+test_tag!(tag16;
+  expect |> Read1; // umapped pair read1
+  vargs ... 0 => None, 0 => None;
+  vargs ... 150 => Upstream, 75 => Upstream, 75 => Upstream;
+);
+
+// test downstream
+test_tag!(tag20; // downstream same position both reads
+  expect |> None;
   vargs ... 10100 => Downstream, 10050 => Downstream;
+  vargs ... 10100 => Downstream, 10075 => Downstream;
+);
+
+test_tag!(tag21; // maximum read1
+  expect |> Read1;
   vargs ... 10150 => Downstream, 10075 => Downstream;
+  vargs ... 10100 => Downstream, 10050 => Downstream;
+);
+
+test_tag!(tag22; // maximum read2
+  expect |> Read2;
+  vargs ... 10150 => Downstream, 10075 => Downstream;
+  vargs ... 10100 => Downstream, 10250 => Downstream;
+);
+
+test_tag!(tag23; // resilient secondary alignments
+  expect |> Read1;
+  vargs ... 10150 => Downstream, 10075 => Downstream, 0 => None;
+  vargs ... 10100 => Downstream, 10050 => Downstream, 100 => Upstream;
+);
+
+test_tag!(tag24;
+  expect |> Read2; // alined same position physical read
+  vargs ... 10100 => Downstream, 10050 => Downstream, 0 => None, 0 => Palindromic;
+  vargs ... 10150 => Downstream, 10075 => Downstream, 10075 => Downstream;
+);
+
+test_tag!(tag25; // umapped pair read2
+  expect |> Read2;
+  vargs ... 10150 => Downstream, 1075 => Downstream, 1075 => Downstream;
+  vargs ... 0 => None, 0 => None;
+);
+
+test_tag!(tag26;
+  expect |> Read1; // umapped pair read1
+  vargs ... 0 => None, 0 => None;
+  vargs ... 10150 => Downstream, 1075 => Downstream, 1075 => Downstream;
 );
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
