e87b0a51abd00c64bf5ad429bad1d02644b5d1ef
DanielRivasMD <danielrivasmd@gmail.com>
2021-08-30 12:06:51 +0200

    patch test determine edge cigar boundry mobile element chimeric read

--- tests/tests.rs
+++ tests/tests.rs
@@ -19,7 +19,11 @@ macro_rules! load_me_anchor {
   ( $me_chimeric_read: expr, $position: expr, $orientation: tt ) => {
     let mut me_anchor = MEAnchor::new();
     me_anchor.orientation = OrientationEnum::$orientation;
-    me_anchor.position = $position;
+    match OrientationEnum::$orientation {
+      OrientationEnum::Upstream => me_anchor.cigar.left_boundry = $position,
+      OrientationEnum::Downstream => me_anchor.cigar.right_boundry = $position,
+      _ => (),
+    }
     $me_chimeric_read.me_read.push(me_anchor);
   };
 }
